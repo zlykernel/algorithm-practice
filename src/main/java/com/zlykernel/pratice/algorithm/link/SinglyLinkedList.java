03b3b25fa045e6312c42b0c98f9813b8048c13ab
zhaoliangyuan <zhaoliangyuan@mobimagic.com>
2020-04-23 22:04:50 +0800

    link list base

---
+++ src/main/java/com/zlykernel/pratice/algorithm/link/SinglyLinkedList.java
@@ -0,0 +1,10 @@
+package com.zlykernel.pratice.algorithm.link;
+
+/**
+ * @author zhaoliangyuan
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020年04月23日 11:07:00
+ */
+public class SinglyLinkedList {
+}
