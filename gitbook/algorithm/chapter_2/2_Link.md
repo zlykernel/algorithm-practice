d4d0e0900911cd97f2bcab1f50969254f37d58ee
zkernel <zly@git.com>
2020-04-26 22:28:25 +0800

    add link revese comments

--- gitbook/algorithm/chapter_2/2_Link.md
+++ gitbook/algorithm/chapter_2/2_Link.md
@@ -25,14 +25,23 @@
    ![](../image/img2-2-2.jpg) 
    + 双向链表
    ![](../image/img2-2-3.jpg) 
+
++ 链表实践技巧
+    
+    + 使用哨兵简化实现难度
+    + 建议大家在实现之前的思考时间不要太长。一是先用自己能想到的暴力方法实现试试。另外就是在一定时间内(比如半个到一个小时)实在想不到就要在网上搜搜答案。有的算法，比如链表中环的检测，的最优解法还是挺巧妙的，一般来说不是生想就能想到的
    
 **思考**
 + 快慢指针法
 + cpu缓存机制
 
 **实践**
-+ 实现链表[代码例子](/src/main/java/com/zlykernel/pratice/algorithm/link/SinglyLinkedList.java) 
-+ 链表反转 [代码例子](../../algorithm/others/README.md) 
++ 实现链表[代码例子(完成)](/src/main/java/com/zlykernel/pratice/algorithm/link/SinglyLinkedList.java) 
++ 单向链表反转 [代码例子(完成)](../../algorithm/others/README.md) 
++ 双向链表反转 [代码例子(完成)](../../algorithm/others/README.md) 
 + 链表是否有环 [代码例子](../../algorithm/others/README.md) 
 + 链表实现 LRU 缓存淘汰算法 [代码例子](../../algorithm/others/README.md) 
 + 判断一个字符串是否是回文字符串 [代码例子](../../algorithm/others/README.md) 
++ 两个有序的链表合并 [代码例子](../../algorithm/others/README.md)
++ 删除链表倒数第 n 个结点 [代码例子](../../algorithm/others/README.md)
++ 求链表的中间结点 [代码例子](../../algorithm/others/README.md)
--- src/main/java/com/zlykernel/pratice/algorithm/link/SinglyLinkedList.java
+++ src/main/java/com/zlykernel/pratice/algorithm/link/SinglyLinkedList.java
@@ -23,6 +23,14 @@ public class SinglyLinkedList<E> {
     private int size;
 
     /**
+     * 设置环为链表
+     */
+    public void setAnnular(){
+        tail.next=first;
+    }
+
+
+    /**
      * 添加元素
      * @Author zhaoliangyuan
      * @Description //TODO
@@ -46,6 +54,10 @@ public class SinglyLinkedList<E> {
 
     /**
      * 链表反转
+     * 反转本质是改变节点之间的关系
+     * 要找到影响最小的范围(节点数和关系数)
+     * 最小单元思考问题
+     * 否则会增加思考难度
      * @Author zhaoliangyuan
      * @Description //TODO
      * @Date 20:41 2020/4/23
@@ -53,17 +65,36 @@ public class SinglyLinkedList<E> {
      * @return void
      **/
     public void reverse(SinglyLinkedList list){
-        //
+        //当前节点
         Node cur=first;
         if (cur == null){
             return;
         }
         tail = cur;
+        /**
+         *  前驱节点
+         * 为什么要定义前驱节点?
+         * 原因是反转时，前驱节点通过节点自身的属性无法获取，所有要定义临时变量。
+         */
         Node pre=null;
+        /**
+         * 为什么此处要使用cur！=null 作为边界条件而不是cur.next!=null
+         * 因为循环体中
+         */
         while (cur!=null){
+            /**
+             * 此处定义tempNext临时变量的原因？
+             * 因为反转会改变当前节点 next指针，所以要快照该节点内容
+             * 如果不快照，next指针改变为pre节点，后续循环关系断裂。
+             */
             Node tempNext =cur.next;
+            /**
+             * 将后续节点关系改变执行前驱节点
+             */
             cur.next=pre;
+            //设置将当前处理的节点 赋值给前驱节点 以便下一次反转处理使用
             pre=cur;
+            //将洗一次节点赋值给当前节点 这是循环继续的驱动条件
             cur=tempNext;
         }
         first=pre;
@@ -78,21 +109,12 @@ public class SinglyLinkedList<E> {
             sb.append(temp.value.toString());
             sb.append(",");
             temp=temp.next;
+//            System.out.println(sb.toString());
         }
         sb.append("]");
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
-        System.out.println(list.toString());
-        list.reverse(list);
-        System.out.println(list.toString());
-    }
     /**
      * 链表 节点 封装 基础操作结构单元
      * @param <E>
@@ -105,6 +127,42 @@ public class SinglyLinkedList<E> {
             this.value = value;
             this.next = next;
         }
+    }
+
+    public static void main(String[] args) {
+//        testListReverse();
+        testAnnularList();
+    }
+
+    /**
+     *链表基础/反转
+     * 测试方法
+     */
+    private static void testListReverse(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        System.out.println("链表打印："+list.toString());
+        list.reverse(list);
+        System.out.println("链表反转打印："+list.toString());
+    }
+
+    /**
+     * 环形列表 测试方法
+     * 测试方法
+     * @return
+     */
+    private static SinglyLinkedList testAnnularList(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.setAnnular();
+//        System.out.println("环形链表打印："+list.toString());
+        return list;
     }
 }
 
