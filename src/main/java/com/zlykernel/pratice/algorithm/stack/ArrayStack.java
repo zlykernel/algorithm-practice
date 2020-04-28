05a53589614235730bf6a048b4c9d93ac0dbceda
zhaoliangyuan <zhaoliangyuan@mobimagic.com>
2020-04-28 20:50:28 +0800

    add stack day 1

---
+++ src/main/java/com/zlykernel/pratice/algorithm/stack/ArrayStack.java
@@ -0,0 +1,11 @@
+package com.zlykernel.pratice.algorithm.stack;
+
+/**
+ * 栈基于数据
+ * @author zhaoliangyuan
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020年04月28日 10:43:00
+ */
+public class ArrayStack {
+}
---
+++ src/main/java/com/zlykernel/pratice/algorithm/stack/LinkedListStack.java
@@ -0,0 +1,12 @@
+package com.zlykernel.pratice.algorithm.stack;
+
+/**
+ * 栈基于链表
+ * @author zhaoliangyuan
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020年04月28日 10:43:00
+ */
+public class LinkedListStack {
+
+}
