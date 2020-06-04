1dda47df5cdeef57e0e96afd2d2db35b89e7d31f
zhaoliangyuan <zhaoliangyuan@mobimagic.com>
2020-06-04 22:26:07 +0800

    radix day2

--- src/main/java/com/zlykernel/pratice/algorithm/sort/RadixSort.java
+++ src/main/java/com/zlykernel/pratice/algorithm/sort/RadixSort.java
@@ -24,7 +24,10 @@ public class RadixSort {
             }
         }
 
-        // 从个位开始，对数组arr按"指数"进行排序
+        /**
+         * 从个位开始，对数组arr按"指数"进行排序
+         * 对进制(除以1，10，100等)进行整除
+         */
         for (int exp = 1; max / exp > 0; exp *= 10) {
             countingSort(arr, exp);
         }
@@ -34,7 +37,7 @@ public class RadixSort {
      * 计数排序-对数组按照"某个位数"进行排序
      *
      * @param arr
-     * @param exp 指数
+     * @param exp 指数 禁止
      */
     public static void countingSort(int[] arr, int exp) {
         if (arr.length <= 1) {
@@ -44,6 +47,9 @@ public class RadixSort {
         // 计算每个元素的个数
         int[] c = new int[10];
         for (int i = 0; i < arr.length; i++) {
+            /**
+             *
+             */
             c[(arr[i] / exp) % 10]++;
         }
 
@@ -55,7 +61,9 @@ public class RadixSort {
         // 临时数组r，存储排序之后的结果
         int[] r = new int[arr.length];
         for (int i = arr.length - 1; i >= 0; i--) {
+            //在对位置上保存数字
             r[c[(arr[i] / exp) % 10] - 1] = arr[i];
+            //该元素对应的个数-1
             c[(arr[i] / exp) % 10]--;
         }
 
