f86e65487957a53ce09416b0bf8bcaf14d3dbed2
zhaoliangyuan <zhaoliangyuan@mobimagic.com>
2020-06-24 18:12:52 +0800

    skiplist day1

---
+++ src/main/java/com/zlykernel/pratice/algorithm/skiplist/SkipList.java
@@ -0,0 +1,12 @@
+package com.zlykernel.pratice.algorithm.skiplist;
+
+/**
+ * 跳表
+ * @author zhaoliangyuan
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020年06月24日 17:12:00
+ */
+public class SkipList {
+
+}
